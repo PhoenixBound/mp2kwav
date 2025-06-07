26c24934bfdd471dbe55cea34b7b76315fc069d7
PhoenixBound <phoenixbound.github@gmail.com>
2025-06-07 01:01:48 -0500

    Add command-line argument parsing
    Some other stuff like cleaning up default clippy warnings, too

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use anyhow::{anyhow, bail, Context};
-use std::path::PathBuf;
+use std::io::Write;
+use std::path::{Path, PathBuf};
 use std::sync::LazyLock;
 
-const NOTE_NAMES: [&'static str; 12] = [
+const NOTE_NAMES: [&str; 12] = [
     "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B",
 ];
 
@@ -14,13 +15,16 @@ static TWELFTH_ROOT_OF_TWO_POWERS: LazyLock<[f64; 12]> = LazyLock::new(|| {
     ]
 });
 
-/// Add a stand-in for a function similar to IEEE-754 `remainder` operations.
-/// The argument is always positive for our purposes so it should be equivalent
+/// Returns lhs.rem_ieee(1.).abs(), where x.rem_ieee(y) is the IEEE-754
+/// operation "remainder(x, y)" (which isn't available in Rust, seemingly).
+/// May round slightly worse than the real thing, but we don't need super high
+/// accuracy, so it works.
 fn abs_remainder_by_one(lhs: f64) -> f64 {
-    if lhs % 1.0 > 0.5 {
-        1.0 - lhs % 1.0
+    let frac = (lhs % 1.0).abs();
+    if frac > 0.5 {
+        1.0 - frac
     } else {
-        lhs % 1.0
+        frac
     }
 }
 
@@ -69,17 +73,12 @@ fn guess_base_note_from_sample_pitch(sample_pitch: u32, min_rate: u32) -> Option
         best_sample_rate /= 2.;
         base_note -= 12;
     }
-    
-    println!("3: {}", best_sample_rate);
-    
+        
     let best_sample_rate_int = best_sample_rate.round() as u32;
-    println!("4: {} w/ base note {}", best_sample_rate_int, base_note);
     // Verify that this best sample rate + base note actually gives us back the
     // right pitch value
     let pitch_factor = (f64::from(60 - base_note)/12.).exp2() * 1024.;
-    println!("Scaling sample rate up by {} ({})", pitch_factor, pitch_factor / 1024.);
     let calculated_sample_pitch = ((f64::from(best_sample_rate_int) * pitch_factor).round()) as u32;
-    println!("Calculated sample pitch is {} (vs original {})", calculated_sample_pitch, sample_pitch);
     if calculated_sample_pitch == sample_pitch {
         return Some((best_sample_rate_int, base_note.try_into().unwrap()));
     }
@@ -90,7 +89,7 @@ fn guess_base_note_from_sample_pitch(sample_pitch: u32, min_rate: u32) -> Option
     None
 }
 
-fn convert_to_wav(gba_sample: &[u8]) -> Result<(Vec<u8>, u8), anyhow::Error> {
+fn convert_to_wav(gba_sample: &[u8], min_rate: u32) -> Result<(Vec<u8>, u8), anyhow::Error> {
     let sample_size = u32::from_le_bytes(gba_sample[12..16].try_into().unwrap());
     let loop_start = u32::from_le_bytes(gba_sample[8..12].try_into().unwrap());
     let gba_pitch = u32::from_le_bytes(gba_sample[4..8].try_into().unwrap());
@@ -109,7 +108,7 @@ fn convert_to_wav(gba_sample: &[u8]) -> Result<(Vec<u8>, u8), anyhow::Error> {
         bail!("Loop point is out of bounds");
     }
     
-    let (sample_rate, base_note) = guess_base_note_from_sample_pitch(gba_pitch, 10000)
+    let (sample_rate, base_note) = guess_base_note_from_sample_pitch(gba_pitch, min_rate)
         .ok_or_else(|| anyhow!("Couldn't guess sample rate"))?;
     
     let allocation_size = 12usize + 8 + 16 + if looping { 8 + 15 * 4 } else { 0 } + 8 + sample_size_usize;
@@ -196,17 +195,122 @@ fn convert_to_wav(gba_sample: &[u8]) -> Result<(Vec<u8>, u8), anyhow::Error> {
     Ok((wav, base_note))
 }
 
+struct ArgsNormal {
+    in_path: PathBuf,
+    out_path: PathBuf,
+    min_rate: u32,
+    base_note: u8,
+}
+
+enum Args {
+    Normal(ArgsNormal),
+    Help
+}
+
+fn parse_base_note(note: &str) -> Result<u8, anyhow::Error> {
+    let end_of_note_name = note.find(|c: char| {
+        c.is_ascii_digit() || c == '-'
+    }).ok_or(anyhow!("No octave number found in note name '{}'", note))?;
+    
+    let (note_name_str, octave_str) = note.split_at(end_of_note_name);
+    let mut midi_note: i16 = match note_name_str {
+        "C" | "c" => 0,
+        "C#" | "c#" | "Db" | "db" => 1,
+        "D" | "d" => 2,
+        "D#" | "d#" | "Eb" | "eb" => 3,
+        "E" | "e" => 4,
+        "F" | "f" => 5,
+        "F#" | "f#" | "Gb" | "gb" => 6,
+        "G" | "g" => 7,
+        "G#" | "g#" | "Ab" | "ab" => 8,
+        "A" | "a" => 9,
+        "A#" | "a#" | "Bb" | "bb" => 10,
+        "B" | "b" => 11,
+        _ => bail!("Invalid note name '{}'", note_name_str),
+    };
+    let octave_int: i16 = octave_str.parse()
+        .with_context(|| format!("Couldn't convert '{}' to integer", octave_str))?;
+    midi_note += (octave_int + 1i16) * 12;
+    midi_note.try_into().with_context(|| format!("Calculated note ({}) out of byte range", midi_note))
+}
+
+fn parse_args(parser: &mut lexopt::Parser) -> Result<Args, anyhow::Error> {
+    use lexopt::prelude::*;
+    
+    let mut in_path: Option<PathBuf> = None;
+    let mut out_path: Option<PathBuf> = None;
+    let mut min_rate = 10000u32;
+    let mut base_note = 60u8;
+    
+    while let Some(arg) = parser.next()? {
+        match arg {
+            Short('h') | Short('?') | Long("help") => {
+                return Ok(Args::Help);
+            }
+            Short('m') | Long("min-rate") => {
+                min_rate = parser.value()?.parse()?;
+            }
+            Short('b') | Long("base") => {
+                base_note = parser.value()?.parse_with(parse_base_note)?;
+            }
+            Value(val) if in_path.is_none() => {
+                let val_path: &Path = val.as_ref();
+                in_path = Some(val_path.to_path_buf());
+            }
+            Value(val) if out_path.is_none() => {
+                let val_path: &Path = val.as_ref();
+                out_path = Some(val_path.to_path_buf());
+            }
+            _ => return Err(arg.unexpected().into()),
+        }
+    }
+    
+    Ok(Args::Normal(ArgsNormal {
+        in_path: in_path.ok_or(anyhow!("No input file path provided"))?,
+        out_path: out_path.ok_or(anyhow!("No output file path provided"))?,
+        min_rate,
+        base_note,
+    }))
+}
+
+fn print_usage(bin_name: &str) {
+    let stderr = std::io::stderr();
+    let mut handle = stderr.lock();
+    // Ignore potential errors writing to stderr... I don't think anyone will
+    // miss this message if they happen to redirect stderr to a file and fail at
+    // converting a gazillion samples
+    _ = writeln!(handle, "mp2kwav - WAV sample converter for GBA MusicPlayer2000 (BIOS sound engine) samples");
+    _ = writeln!(handle, "Usage:");
+    _ = writeln!(handle, "    {} [-h?|--help]", bin_name);
+    _ = writeln!(handle, "    {} [-m|--min-rate=<number>] <gba-sample.bin> <output-wav-sample.wav>", bin_name);
+    _ = writeln!(handle, "    {} [-b|--base=<note>] <wav-sample.wav> <output-gba-sample.bin>", bin_name);
+    _ = writeln!(handle, "Options:");
+    _ = writeln!(handle, "    -b, --base: Set base note for the sample when converting .wav --> .bin (default: C4)");
+    _ = writeln!(handle, "    -m, --min-rate: Set permitted WAV sample rate to lie in the range [n, 2n) (default: 10000)");
+    _ = writeln!(handle, "    -h, -?, --help: Display this help message");
+    _ = writeln!(handle);
+    _ = writeln!(handle, "See NOTICES.txt for copyright notices from all used libraries.");
+}
+
 fn main() -> Result<(), anyhow::Error> {
-    // TODO: command-line argument parsing
-    let in_path = PathBuf::from("../m3-732460.bin");
-    let out_path = PathBuf::from("../m3-732460.wav");
-    let error_if_outfile_exists = false;
+    let mut args_parser = lexopt::Parser::from_env();
+    
+    let args = parse_args(&mut args_parser)?;
+    if let Args::Help = args {
+        print_usage(args_parser.bin_name().unwrap_or("mp2kwav"));
+        return Ok(());
+    }
+    let Args::Normal(an) = args else { unreachable!() };
+    let in_path = an.in_path;
+    let out_path = an.out_path;
+    let min_rate = an.min_rate;
+    let _base_note = an.base_note;
     
     // Load all data from in_path
     let infile_data = std::fs::read(&in_path)
         .with_context(|| format!("Failed to read sample from {:?}", in_path))?;
     
-    let (wav_data, base_note) = convert_to_wav(&infile_data[..])
+    let (wav_data, base_note) = convert_to_wav(&infile_data[..], min_rate)
         .with_context(|| format!("Failed to convert sample data from file {:?}", in_path))?;
     
     println!("Base note: {}{}", NOTE_NAMES[usize::from(base_note) % 12], base_note / 12 - 1);
